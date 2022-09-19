043d86368e14972cb3c0119517cc30df62d87ce4
rnoguer22 <rnogugon@myuax.com>
2022-09-20 00:05:22 +0200

    Programacion funcional para una mayor ordenacion de codigo

--- Markov.py
+++ Markov.py
@@ -16,41 +16,23 @@ data["gold_price_change"] = data["gold_price_usd"].diff()
 # Restrict the data to later than 2008 Jan 01.
 data = data[data["datetime"] >= pd.to_datetime("2008-01-01")]
 
+
 # Plot the daily gold prices as well as the daily change.
-plt.figure(figsize = (15, 10))
-plt.subplot(2,1,1)
-plt.plot(data["datetime"], data["gold_price_usd"])
-plt.xlabel("datetime")
-plt.ylabel("gold price (usd)")
-plt.grid(True)
-plt.subplot(2,1,2)
-plt.plot(data["datetime"], data["gold_price_change"])
-plt.xlabel("datetime")
-plt.ylabel("gold price change (usd)")
-plt.grid(True)
-plt.show()
-data = pd.read_csv(base_dir)
+def grafico1():
+    plt.figure(figsize = (15, 10))
+    plt.subplot(2,1,1)
+    plt.plot(data["datetime"], data["gold_price_usd"])
+    plt.xlabel("datetime")
+    plt.ylabel("gold price (usd)")
+    plt.grid(True)
+    plt.subplot(2,1,2)
+    plt.plot(data["datetime"], data["gold_price_change"])
+    plt.xlabel("datetime")
+    plt.ylabel("gold price change (usd)")
+    plt.grid(True)
+    plt.show()
 
-# Convert the datetime from str to datetime object.
-data["datetime"] = pd.to_datetime(data["datetime"])
-
-# Determine the daily change in gold price.
-data["gold_price_change"] = data["gold_price_usd"].diff()
-
-# Restrict the data to later than 2008 Jan 01.
-data = data[data["datetime"] >= pd.to_datetime("2008-01-01")]
-
-plt.figure(figsize = (15, 10))
-plt.subplot(2,1,1)
-plt.plot(data["datetime"], data["gold_price_usd"])
-plt.xlabel("datetime")
-plt.ylabel("gold price (usd)")
-plt.grid(True)
-plt.subplot(2,1,2)
-plt.plot(data["datetime"], data["gold_price_change"])
-plt.xlabel("datetime")
-plt.ylabel("gold price change (usd)")
-plt.grid(True)
+grafico1()
 
 
 # Use the daily change in gold price as the observed measurements X.
@@ -62,40 +44,29 @@ model.fit(X)
 Z = model.predict(X)
 states = pd.unique(Z)
 
-print("Unique states:")
-print(states)
 
-print("\nStart probabilities:")
-print(model.startprob_)
+def grafico2():
+    plt.figure(figsize = (15, 10))
+    plt.subplot(2,1,1)
+    for i in states:
+        want = (Z == i)
+        x = data["datetime"].iloc[want]
+        y = data["gold_price_usd"].iloc[want]
+        plt.plot(x, y, '.')
+    plt.legend(states, fontsize=16)
+    plt.grid(True)
+    plt.xlabel("datetime", fontsize=16)
+    plt.ylabel("gold price (usd)", fontsize=16)
+    plt.subplot(2,1,2)
+    for i in states:
+        want = (Z == i)
+        x = data["datetime"].iloc[want]
+        y = data["gold_price_change"].iloc[want]
+        plt.plot(x, y, '.')
+    plt.legend(states, fontsize=16)
+    plt.grid(True)
+    plt.xlabel("datetime", fontsize=16)
+    plt.ylabel("gold price change (usd)", fontsize=16)
+    plt.show()
 
-print("\nTransition matrix:")
-print(model.transmat_)
-
-print("\nGaussian distribution means:")
-print(model.means_)
-
-print("\nGaussian distribution covariances:")
-print(model.covars_)
-
-plt.figure(figsize = (15, 10))
-plt.subplot(2,1,1)
-for i in states:
-    want = (Z == i)
-    x = data["datetime"].iloc[want]
-    y = data["gold_price_usd"].iloc[want]
-    plt.plot(x, y, '.')
-plt.legend(states, fontsize=16)
-plt.grid(True)
-plt.xlabel("datetime", fontsize=16)
-plt.ylabel("gold price (usd)", fontsize=16)
-plt.subplot(2,1,2)
-for i in states:
-    want = (Z == i)
-    x = data["datetime"].iloc[want]
-    y = data["gold_price_change"].iloc[want]
-    plt.plot(x, y, '.')
-plt.legend(states, fontsize=16)
-plt.grid(True)
-plt.xlabel("datetime", fontsize=16)
-plt.ylabel("gold price change (usd)", fontsize=16)
-plt.show()
+grafico2()
