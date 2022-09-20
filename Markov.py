047f4ce370329755b2b9d600029f329de76c6153
rnoguer22 <rnogugon@myuax.com>
2022-09-20 12:07:13 +0200

    El dataset del chocolate no es valido Implementacion de otro dataset, que se basa en el crecimiento de arboles

--- Markov.py
+++ Markov.py
@@ -3,28 +3,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from hmmlearn import hmm
 
-dataset = "chocolate_ratings.csv"
+dataset = "Tree_growth.csv"
 
 data = pd.read_csv(dataset)
 
-# Transformamos la fecha de una cadena de texto a un datetime object
-data["Review Date"] = pd.to_datetime(data["Review Date"])
+data.dropna(axis = 0)
 
-# Determinamos el cambio en la valoracion
-data["Rating change"] = data["Rating"].diff()
+# Determinamos el cambio teniendo en cuenta el valor anterior
+data["TL Change"] = data["TL"].diff()
 
 # Funcion para elaborar un grafico de la valoracion y la valoracion una vez hecho el cambio
 def grafico1():
     plt.figure(figsize = (15, 10))
     plt.subplot(2,1,1)
-    plt.plot(data["datetime"], data["gold_price_usd"])
-    plt.xlabel("datetime")
-    plt.ylabel("gold price (usd)")
+    plt.plot(data["Review Date"], data["Rating"])
+    plt.xlabel("Fecha")
+    plt.ylabel("Valoracion")
     plt.grid(True)
     plt.subplot(2,1,2)
-    plt.plot(data["datetime"], data["gold_price_change"])
-    plt.xlabel("datetime")
-    plt.ylabel("gold price change (usd)")
+    plt.plot(data["Review Date"], data["Rating Change"])
+    plt.xlabel("Fecha")
+    plt.ylabel("Valoracion cambiada")
     plt.grid(True)
     plt.show()
 
@@ -32,7 +31,7 @@ grafico1()
 
 
 # Use the daily change in gold price as the observed measurements X.
-X = data[["gold_price_change"]].values
+X = data[["Rating Change"]].values
 # Build the HMM model and fit to the gold price change data.
 model = hmm.GaussianHMM(n_components = 3, covariance_type = "diag", n_iter = 50, random_state = 42)
 model.fit(X)
@@ -46,23 +45,23 @@ def grafico2():
     plt.subplot(2,1,1)
     for i in states:
         want = (Z == i)
-        x = data["datetime"].iloc[want]
-        y = data["gold_price_usd"].iloc[want]
+        x = data["Review Date"].iloc[want]
+        y = data["Rating"].iloc[want]
         plt.plot(x, y, '.')
     plt.legend(states, fontsize=16)
     plt.grid(True)
-    plt.xlabel("datetime", fontsize=16)
-    plt.ylabel("gold price (usd)", fontsize=16)
+    plt.xlabel("Review Date", fontsize=16)
+    plt.ylabel("Rating", fontsize=16)
     plt.subplot(2,1,2)
     for i in states:
         want = (Z == i)
-        x = data["datetime"].iloc[want]
-        y = data["gold_price_change"].iloc[want]
+        x = data["Review Date"].iloc[want]
+        y = data["Rating Change"].iloc[want]
         plt.plot(x, y, '.')
     plt.legend(states, fontsize=16)
     plt.grid(True)
-    plt.xlabel("datetime", fontsize=16)
-    plt.ylabel("gold price change (usd)", fontsize=16)
+    plt.xlabel("Review Date", fontsize=16)
+    plt.ylabel("Rating Change", fontsize=16)
     plt.show()
 
 grafico2()
