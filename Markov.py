35a68459a4d3a3d820a749e32938316beb3974b8
rnoguer22 <rnogugon@myuax.com>
2022-09-20 11:26:04 +0200

    Primera implementacion de la cadena de markov al dataset del chocolate

--- Markov.py
+++ Markov.py
@@ -3,21 +3,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from hmmlearn import hmm
 
-base_dir = "https://github.com/natsunoyuki/Data_Science/blob/master/gold/gold/gold_price_usd.csv?raw=True"
+dataset = "chocolate_ratings.csv"
 
-data = pd.read_csv(base_dir)
+data = pd.read_csv(dataset)
 
-# Convert the datetime from str to datetime object.
-data["datetime"] = pd.to_datetime(data["datetime"])
+# Transformamos la fecha de una cadena de texto a un datetime object
+data["Review Date"] = pd.to_datetime(data["Review Date"])
 
-# Determine the daily change in gold price.
-data["gold_price_change"] = data["gold_price_usd"].diff()
+# Determinamos el cambio en la valoracion
+data["Rating change"] = data["Rating"].diff()
 
-# Restrict the data to later than 2008 Jan 01.
-data = data[data["datetime"] >= pd.to_datetime("2008-01-01")]
-
-
-# Plot the daily gold prices as well as the daily change.
+# Funcion para elaborar un grafico de la valoracion y la valoracion una vez hecho el cambio
 def grafico1():
     plt.figure(figsize = (15, 10))
     plt.subplot(2,1,1)
