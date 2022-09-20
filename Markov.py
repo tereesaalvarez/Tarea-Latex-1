665d0feeb05ec9826cf3e52ddbb75873aea9f0cc
rnoguer22 <rnogugon@myuax.com>
2022-09-20 12:36:25 +0200

    Finalizacion del codigo que genera el grafico

--- Markov.py
+++ Markov.py
@@ -12,56 +12,19 @@ data.dropna(axis = 0)
 # Determinamos el cambio teniendo en cuenta el valor anterior
 data["TL Change"] = data["TL"].diff()
 
-# Funcion para elaborar un grafico de la valoracion y la valoracion una vez hecho el cambio
-def grafico1():
+# Funcion para elaborar un grafico del dataset aplicando cadenas de markov
+def grafico():
     plt.figure(figsize = (15, 10))
     plt.subplot(2,1,1)
-    plt.plot(data["Review Date"], data["Rating"])
-    plt.xlabel("Fecha")
-    plt.ylabel("Valoracion")
+    plt.plot(data["N"], data["TL"])
+    plt.xlabel("Id")
+    plt.ylabel("Longitud total del brote")
     plt.grid(True)
     plt.subplot(2,1,2)
-    plt.plot(data["Review Date"], data["Rating Change"])
-    plt.xlabel("Fecha")
-    plt.ylabel("Valoracion cambiada")
+    plt.plot(data["N"], data["TL Change"])
+    plt.xlabel("Id")
+    plt.ylabel("Prediccion Longitud total brote")
     plt.grid(True)
     plt.show()
 
-grafico1()
-
-
-# Use the daily change in gold price as the observed measurements X.
-X = data[["Rating Change"]].values
-# Build the HMM model and fit to the gold price change data.
-model = hmm.GaussianHMM(n_components = 3, covariance_type = "diag", n_iter = 50, random_state = 42)
-model.fit(X)
-# Predict the hidden states corresponding to observed X.
-Z = model.predict(X)
-states = pd.unique(Z)
-
-
-def grafico2():
-    plt.figure(figsize = (15, 10))
-    plt.subplot(2,1,1)
-    for i in states:
-        want = (Z == i)
-        x = data["Review Date"].iloc[want]
-        y = data["Rating"].iloc[want]
-        plt.plot(x, y, '.')
-    plt.legend(states, fontsize=16)
-    plt.grid(True)
-    plt.xlabel("Review Date", fontsize=16)
-    plt.ylabel("Rating", fontsize=16)
-    plt.subplot(2,1,2)
-    for i in states:
-        want = (Z == i)
-        x = data["Review Date"].iloc[want]
-        y = data["Rating Change"].iloc[want]
-        plt.plot(x, y, '.')
-    plt.legend(states, fontsize=16)
-    plt.grid(True)
-    plt.xlabel("Review Date", fontsize=16)
-    plt.ylabel("Rating Change", fontsize=16)
-    plt.show()
-
-grafico2()
+grafico()
